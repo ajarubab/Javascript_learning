91f72eb3bd60985ac6574da12bfa3488c8493cf5
Raja kumar prasad <ajarubab@gmail.com>
2026-02-19 20:57:27 +0530

    IIFE + HOF + Callback Hell

--- 04_Function/functions.js
+++ 04_Function/functions.js
@@ -1,20 +1,21 @@
+/** 
 console.log("*************************************************");
 
-function fun1() {
+function fun1(){
     console.log("Function one here")
 }
 fun1()
 
 //--------------------------------------------------
 
-function fun2() {
+function fun2(){
     return "Hello fun2 message here";
 }
 console.log(fun2());
 
 console.log("*************************************************");
 
-function fun3(name) {
+function fun3(name){
     return `hi this is ${name}`;
 }
 
@@ -23,28 +24,28 @@ console.log(fun3("shyam"));
 
 //--------------------------------------------------
 
-function fun4(a, b) {
-    a = a * b;
+function fun4(a,b){
+    a=a*b;
     b = a - b;
-    return { a, b };
+    return {a, b};
 }
 
-console.log(fun4(4, 6));
+console.log(fun4(4,6));
 
 console.log("*************************************************");
 
 // Named Function Expression
 
-let fact = function factorial(n) {
-    if (n === 0) return 1;
-    return n * factorial(n - 1);
+let fact = function factorial(n){
+    if(n===0) return 1;
+    return n * factorial(n-1);
 }
 
-console.log(`Factorial of 5 = `, fact(5))
+console.log(`Factorial of 5 = `,fact(5))
 
 //--------------------------------------------------
 
-let res = function (country) {
+let res = function(country){
     return `I love ${country}`;
 }
 
@@ -57,17 +58,17 @@ console.log("*************************************************");
 const greet = () => "Welcome to arrow function section";    // No parameter arrow function
 console.log(greet())
 
-const cube = dig => `Cube of ${dig} is ${dig * dig * dig}`  // for single parameter function
+const cube = dig => `Cube of ${dig} is ${dig*dig*dig}`  // for single parameter function
 console.log(cube(3));
 
-const mul_res = (num1, num2) => { return num1 * num2 };     // for function having more than one parameter
-console.log(mul_res(25, 4));
+const mul_res = (num1, num2) => {return num1*num2};     // for function having more than one parameter
+console.log(mul_res(25,4));
 
 console.log("*************************************************");
 
 // IIFE ( immediately Invoked Function Expression) used for scope isolation
 
-(function () {
+(function(){
     console.log("IIFE Execution done.")
 })();
 
@@ -75,63 +76,63 @@ console.log("*************************************************");
 
 // Callback Function
 
-function test(cbf) {
+function test(cbf){
     console.log("Start");
     cbf();
 }
 
-test(function () {
+test(function(){
     console.log("End");
 });
 
 //--------------------------------
 
-function teacher(student) {
+function teacher(student){
     student();
 }
 
-teacher(function () {
+teacher(function(){
     console.log("Present Ma'am");
 });
 
 //-----------------------------------
 
-function sayHello(cbfn) {
+function sayHello(cbfn){
     cbfn();
 }
 
-sayHello(() => console.log("Hello world"));
+sayHello(()=>console.log("Hello world"));
 
 //-----------------------------------
 
-function sayHi(clb_fn) {
+function sayHi(clb_fn){
     return clb_fn();
 }
 
-sayHi(() => console.log("Hi world"));
+sayHi(()=>console.log("Hi world"));
 
 //-----------------------------------
 
-function sayYo(clbk_fn) {
+function sayYo(clbk_fn){
     return clbk_fn();
 }
 
-let new_res = sayYo(() => `Yo Yo Honey Singh`);
+let new_res = sayYo(()=>`Yo Yo Honey Singh`);
 console.log(new_res);
 
 //-----------------------------------
 
-function abhivadan(name, cb_fn) {
+function abhivadan(name, cb_fn){
     cb_fn(name);
 }
 
-abhivadan("Ram", function (n) {
-    console.log("Jai Shree", n);
+abhivadan("Ram", function(n){
+    console.log("Jai Shree",n);
 });
 
 //-----------------------------------
 
-function namaste(person, clbf) {
+function namaste(person, clbf){
     return clbf(person);
 }
 
@@ -140,19 +141,19 @@ console.log(namah);
 
 //-----------------------------------
 
-function color(a, b, op) {
-    return op(a, b);
+function color(a,b,op){
+    return op(a,b);
 }
 
-let output = color(2, 4, (x, y) => x + y);
+let output = color(2,4,(x,y) => x+y);
 console.log(output);
 
 console.log("*************************************************");
 
 // HOF without callback( Hogher Order Function ) : Either takes function as Argumaent or return a function
 
-function multiplier(factor) {
-    return function (num) {
+function multiplier(factor){
+    return function(num){
         return num * factor;
     };
 }
@@ -166,11 +167,11 @@ console.log(dbl(5));
 // HOF due to : operationn as an argument
 // Callback due to : (x,y) => x - y
 
-function operate(a, b, operation) {
+function operate(a, b, operation){
     return operation(a, b);
 }
 
-let result = operate(5, 2, (x, y) => x - y);
+let result = operate(5, 2, (x,y) => x - y);
 console.log(result);
 
 
@@ -178,6 +179,53 @@ console.log("*************************************************");
 
 // Anonymous function [ mostly used in callbacks ]
 
-setTimeout(function () {
+setTimeout(function(){
     console.log("i am anonymous")
 }, 3000);                           // 3000 means 3 seconds
+
+*/
+
+
+console.log("******************* IIFE + HOF + Callback Hell ******************************");
+
+(function () {
+    function login(username, password, callback_fun) {
+
+        setTimeout(() => {
+            console.log("Usernme : ")
+
+            setTimeout(() => {
+                console.log(username)
+
+                setTimeout(() => {
+                    console.log("Password : ")
+
+                    setTimeout(() => {
+                        console.log(password)
+
+                        setTimeout(() => {
+                            console.log("Validating....")
+
+                            setTimeout(() => {
+                                let areCorrectCred;
+                                if (username === "Raja" && password === 112233) {
+                                    areCorrectCred = true;
+                                } else {
+                                    areCorrectCred = false;
+                                }
+                                callback_fun(areCorrectCred);
+                            }, 3000);
+                        }, 1000);
+                    }, 3000);
+                }, 1000);
+            }, 3000);
+        }, 1000);
+    }
+    login("Raja", 112233, (okCred) => {
+        if (okCred) {
+            console.log("Login Sucessfull");
+        } else {
+            console.log("Login failed, Wrong Credentials");
+        }
+    })
+})();
